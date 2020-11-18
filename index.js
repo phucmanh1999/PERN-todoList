7cf8485f9121e9ce3ec439bdb474eb19248fe3e1
Phuc Manh <phucmanh1999@gmail.com>
2020-11-18 16:23:51 +0600

    run server

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(5000, ()=>{
     console.log("server has started on port 5000 ...");
 
 })
+
+
+/*
+CREATE TABLE todo(
+    todo_id SERIAL PRIMARY KEY,
+    descriptopn VARCHAR(255)
+);
+*/
